0f39539960e9303278aae6e575910d62e1279192
1 <Anasko162@Mail.ru>
2022-11-07 01:29:54 +0300

    Задача тест

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-int clientOS =0;
+int clientOS =1;
 if (clientOS==1)
     System.out.println("Установите версию приложения для iOS по ссылке");
-        else {
-        System.out.println("Установите версию приложения для Android по ссылке");
-        }
+        else
+    System.out.println("Установите версию приложения для Android по ссылке");
+ int clientDeviceYear = 2015;
+        boolean DiviceYearsUnder15 = clientDeviceYear <=2015;
+        if (DiviceYearsUnder15)
+     System.out.println("Установи облегченную версию приложения для iOS по ссылке:");
+ if (clientDeviceYear > 2015) { System.out.println("Установите версию приложения для iOS по ссылке");
+     boolean DiviceOlder2015 = clientDeviceYear > 2015;
+     if (DiviceOlder2015)
+         System.out.println("Установите версию для Android по ссылке.");
+     else { System.out.println("Установите облегченную версию для Android по ссылке");
+         int Year = 100;
+         if (VesokosniyGod)
+  }
+ }
 }
-    }
+}
